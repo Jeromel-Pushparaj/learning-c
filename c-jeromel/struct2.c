0e0971b4ed6717eeec677eb7638edbe2001fcab1
jeormel <jeromal03@gmail.com>
2023-07-01 12:31:25 +0530

    testing

--- c-jeromel/struct2.c
+++ c-jeromel/struct2.c
@@ -6,12 +6,12 @@ struct dob{
 int main(){
   struct dob p[3];
   int i;
-  for(i=1;i<4;i++){
+  for(i=1;i<5;i++){
     printf("Enter DOB date month year:");
     scanf("%d%d%d",&p[i].d,&p[i].m,&p[i].y);
   }
   for (i=1;i<4;i++){
     printf("person%d:%d.%d.%d\n",i,p[i].d,p[i].m,p[i].y);
   }
-  
+
 }
