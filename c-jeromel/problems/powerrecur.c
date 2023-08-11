66aec100c25f6123e6e6d98e8eb3f3e23c0b4bfb
Jeromel Pushparaj <116452699+Jeormel@users.noreply.github.com>
2023-08-11 17:59:38 +0530

    recuresive functions studied

---
+++ c-jeromel/problems/powerrecur.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+// Function to calculate power using recursion
+double power(double base, int exponent) {
+    if (exponent == 0) {
+        return 1;
+    } else if (exponent > 0) {
+        return base * power(base, exponent - 1);
+    } else {
+        return 1 / (base * power(base, -exponent - 1));
+    }
+}
+
+int main() {
+    double base;
+    int exponent;
+
+    printf("Enter the base: ");
+    scanf("%lf", &base);
+
+    printf("Enter the exponent: ");
+    scanf("%d", &exponent);
+
+    double result = power(base, exponent);
+    printf("Result: %lf\n", result);
+
+    return 0;
+}
